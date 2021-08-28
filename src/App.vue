cfbf8b93a4f9736c54c9389a4dce2c10d4351e53
Richard Toth <65166879+richardtoth94@users.noreply.github.com>
2021-08-28 20:52:14 +0100

    start of portolio page to get it live

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,23 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-8">
-        <div class="card">
-          Hello
+        <img src="/profile_photo.jpeg" alt="Profile Photo" class="img-fluid profile_image">
+        <div class="text-card mt-3">
+          <h1>Richard Toth</h1>
+          <h4>Web Developer</h4>
+        </div>
+        <div class="socials">
+          <a href="https://www.linkedin.com/in/richard-toth-57508012a/" target="_blank">
+            <font-awesome-icon :icon="['fab', 'linkedin']" />
+          </a>
+
+          <a href="https://twitter.com/richardchad12" target="_blank">
+            <font-awesome-icon :icon="['fab', 'twitter']" />
+          </a>
+
+          <a href="https://github.com/richardtoth94" target="_blank">
+            <font-awesome-icon :icon="['fab', 'github']" />
+          </a>
         </div>
       </div>
     </div>
@@ -31,6 +46,30 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 100px;
+}
+
+.socials {
+  display: flex; 
+  justify-content: center;
+  align-items: center;
+}
+
+.socials a {
+  margin: 10px 20px 0 20px;
+}
+
+.profile_image {
+  width: 160px;
+  height: 160px;
+  border-radius: 100%;
+}
+
+h1, h4, a, svg {
+  color: white;
+}
+
+a {
+  font-size: 24px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+// Bootstrap
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-createApp(App).mount('#app')
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faLinkedin, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faLinkedin, faTwitter, faGithub);
+
+createApp(App)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')
